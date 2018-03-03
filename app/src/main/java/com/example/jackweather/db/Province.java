5889ab9698c928e3b762863782ca9d28bb80efda
jack0246 <1960486253@qq.com>
2018-03-04 00:28:49 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/jackweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.jackweather.db;
+
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 19604 on 3/4/2018.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
